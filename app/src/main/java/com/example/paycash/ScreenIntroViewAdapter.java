ee1db8558f65e8e70a991ddff4271af883fcdbcf
yandiio <yandifenanda95@gmail.com>
2019-07-28 11:43:07 +0700

    persuade

--- app/src/main/java/com/example/paycash/ScreenIntroViewAdapter.java
+++ app/src/main/java/com/example/paycash/ScreenIntroViewAdapter.java
@@ -1,4 +1,67 @@
 package com.example.paycash;
 
-public class ScreenViewAdapter {
+import android.content.Context;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScreenIntroViewAdapter extends PagerAdapter {
+
+    Context mContext;
+    List<ScreenView> mListScreen;
+
+
+    public ScreenIntroViewAdapter(Context mContext, List<ScreenView>mListScreen) {
+        this.mContext = mContext;
+        this.mListScreen = mListScreen;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layoutScreen = inflater.inflate(R.layout.adapter_layout,null);
+
+        ImageView imageView = layoutScreen.findViewById(R.id.imageView) ;
+        TextView title = layoutScreen.findViewById(R.id.intro_title);
+        TextView description = layoutScreen.findViewById(R.id.intro_desc);
+
+        title.setText(mListScreen.get(position).getTitle());
+        description.setText(mListScreen.get(position).getDescription());
+        imageView.setImageResource(mListScreen.get(position).getImageView());
+
+        container.addView(layoutScreen);
+
+        return layoutScreen;
+
+
+
+//        return super.instantiateItem(container, position);
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+
+
+    @Override
+    public int getCount() {
+        return mListScreen.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+        return view == o;
+    }
 }
--- app/src/main/java/com/example/paycash/ScreenView.java
+++ app/src/main/java/com/example/paycash/ScreenView.java
@@ -1,4 +1,36 @@
 package com.example.paycash;
 
 public class ScreenView {
+    String Description, Title;
+    int ImageView;
+
+    public ScreenView(String title, String description, int imageView) {
+        Title = title;
+        Description = description;
+        ImageView = imageView;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public int getImageView() {
+        return ImageView;
+    }
+
+    public void setImageView(int imageView) {
+        ImageView = imageView;
+    }
 }
