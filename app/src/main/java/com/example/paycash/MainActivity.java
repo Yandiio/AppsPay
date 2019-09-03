5e8f1ae13c9ce62af8f01c7f39fab9f01ddaa70d
Karenina <kareninacasandrar76@gmail.com>
2019-09-03 23:04:05 +0700

    first karenina commit

--- app/src/main/java/com/example/paycash/MainActivity.java
+++ app/src/main/java/com/example/paycash/MainActivity.java
@@ -1,14 +1,8 @@
 package com.example.paycash;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+
     }
 
 }
